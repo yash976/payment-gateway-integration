4e9d94a88b8a8c7da4016c5c2c62e42960f0fbe3
Yash Advani <54671322+yash976@users.noreply.github.com>
2026-04-18 17:15:32 +0530

    Create PaymentService.java

---
+++ src/main/java/com/payment/service/PaymentService.java
@@ -0,0 +1,29 @@
+package com.payment.service;
+
+import com.razorpay.*;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+
+    @Value("${razorpay.key}")
+    private String key;
+
+    @Value("${razorpay.secret}")
+    private String secret;
+
+    public String createOrder(int amount) throws RazorpayException {
+
+        RazorpayClient client = new RazorpayClient(key, secret);
+
+        JSONObject options = new JSONObject();
+        options.put("amount", amount * 100);
+        options.put("currency", "INR");
+
+        Order order = client.orders.create(options);
+
+        return order.toString();
+    }
+}
