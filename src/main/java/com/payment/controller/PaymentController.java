5e694a637bbed0960444aef171b0737c33931d88
Yash Advani <54671322+yash976@users.noreply.github.com>
2026-04-18 17:16:42 +0530

    Update PaymentController.java

--- src/main/java/com/payment/controller/PaymentController.java
+++ src/main/java/com/payment/controller/PaymentController.java
@@ -1,13 +1,20 @@
 package com.payment.controller;
 
+import com.payment.service.PaymentService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/payment")
 public class PaymentController {
 
-    @PostMapping("/test")
-    public String test() {
-        return "Payment API Working";
+    private final PaymentService paymentService;
+
+    public PaymentController(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @PostMapping("/create-order")
+    public String createOrder(@RequestParam int amount) throws Exception {
+        return paymentService.createOrder(amount);
     }
 }
